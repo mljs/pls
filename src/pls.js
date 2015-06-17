8ce88e3cd5b0907e2763984ee0859d304ecf7f06
Jefferson <swrt_9950@hotmail.com>
2015-06-17 16:37:01 +0200

    Change the constructor and created a new function called fit

--- src/pls.js
+++ src/pls.js
@@ -45,114 +45,118 @@ function maxSumColIndex(X) {
 }
 
 /**
- * Construction of the PLS model that takes
- * @param {Matrix} dataset - Dataset to be apply the model
- * @param {Matrix} predictions - Predictions over each case of the dataset
- * @param reload - used internally for load purposes.
+ * Constructor of the PLS model.
+ * @param reload - used for load purposes.
+ * @param model - used for load purposes.
  * @constructor
- * @throws RangeError - if the number of elements in predictions isn't be
- *                      the same to the dataset
  */
-function PLS(dataset, predictions, reload) {
+function PLS(reload, model) {
     if(reload) {
-        this.ymean = dataset.ymean;
-        this.ystd = dataset.ystd;
-        this.PBQ = dataset.PBQ;
-    } else {
-        if(dataset.length !== predictions.length)
-            throw new RangeError("The number of predictions and elements in the dataset must be the same");
-
-        var tolerance = 1e-10;
-        var X = featureNormalize(Matrix(dataset).clone()).result;
-        var resultY = featureNormalize(Matrix(predictions).clone());
-        this.ymean = resultY.means;
-        this.ystd = resultY.std;
-        var Y = resultY.result;
-
-        var rx = X.rows;
-        var cx = X.columns;
-        var ry = Y.rows;
-        var cy = Y.columns;
-
-        if(rx != ry) {
-            throw new Error("dataset cases is not the same as the predictions");
-        }
-
-        var n = Math.max(cx, cy);
-        var T = Matrix.zeros(rx, n);
-        var P = Matrix.zeros(cx, n);
-        var U = Matrix.zeros(ry, n);
-        var Q = Matrix.zeros(cy, n);
-        var B = Matrix.zeros(n, n);
-        var W = P.clone();
-        var k = 0;
-
-        while(norm(Y) > tolerance && k < n) {
-            var transposeX = X.transpose();
-            var transposeY = Y.transpose();
-
-            var tIndex = maxSumColIndex(X.clone().mulM(X));
-            var uIndex = maxSumColIndex(Y.clone().mulM(Y));
-
-            var t1 = X.getColumnVector(tIndex);
-            var u = Y.getColumnVector(uIndex);
-            var t = Matrix.zeros(rx, 1);
-
-            while(norm(t1.clone().sub(t)) > tolerance) {
-                var w = transposeX.mmul(u);
-                w.div(norm(w));
-                t = t1;
-                t1 = X.mmul(w);
-                var q = transposeY.mmul(t1);
-                q.div(norm(q));
-                var u = Y.mmul(q);
-            }
-
-            t = t1;
-            var num = transposeX.mmul(t);
-            var den = (t.transpose().mmul(t))[0][0];
-            var p = num.div(den);
-            var pnorm = norm(p);
-            p.div(pnorm);
-            t.mul(pnorm);
-            w.mul(pnorm);
-
-            num = u.transpose().mmul(t);
-            var b = (num.div(den))[0][0];
-            X.sub(t.mmul(p.transpose()));
-            Y.sub(t.mmul(q.transpose()).mul(b));
-
-
-            T.addColumn(k, t);
-            P.addColumn(k, p);
-            U.addColumn(k, u);
-            Q.addColumn(k, q);
-            W.addColumn(k, w);
-            B[k][k] = b;
-            k++;
-        }
-
-        // NOTE: some variables commented because maybe it's needed
-        // in the future
-
-        k--;
-        n--;
-        //T = T.subMatrix(0, n, 0, k);
-        P = P.subMatrix(0, n, 0, k);
-        //U = U.subMatrix(0, n, 0, k);
-        Q = Q.subMatrix(0, n, 0, k);
-        //W = W.subMatrix(0, n, 0, k);
-        B = B.subMatrix(0, k, 0, k);
-
-        // this.T = T;
-        // this.P = P;
-        // this.U = U;
-        // this.Q = Q;
-        // this.W = W;
-        // this.B = B;
-        this.PBQ = P.mmul(B).mmul(Q.transpose());
+        this.ymean = model.ymean;
+        this.ystd = model.ystd;
+        this.PBQ = model.PBQ;
     }
 }
+
+/**
+ * Function that fit the model with the given data and predictions.
+ * @param {Matrix} trainingSet - Dataset to be apply the model
+ * @param {Matrix} predictions - Predictions over each case of the dataset
+ */
+PLS.prototype.fit = function (trainingSet, predictions) {
+    if(trainingSet.length !== predictions.length)
+        throw new RangeError("The number of predictions and elements in the dataset must be the same");
+
+    var tolerance = 1e-10;
+    var X = featureNormalize(Matrix(trainingSet).clone()).result;
+    var resultY = featureNormalize(Matrix(predictions).clone());
+    this.ymean = resultY.means;
+    this.ystd = resultY.std;
+    var Y = resultY.result;
+
+    var rx = X.rows;
+    var cx = X.columns;
+    var ry = Y.rows;
+    var cy = Y.columns;
+
+    if(rx != ry) {
+        throw new Error("dataset cases is not the same as the predictions");
+    }
+
+    var n = Math.max(cx, cy);
+    var T = Matrix.zeros(rx, n);
+    var P = Matrix.zeros(cx, n);
+    var U = Matrix.zeros(ry, n);
+    var Q = Matrix.zeros(cy, n);
+    var B = Matrix.zeros(n, n);
+    var W = P.clone();
+    var k = 0;
+
+    while(norm(Y) > tolerance && k < n) {
+        var transposeX = X.transpose();
+        var transposeY = Y.transpose();
+
+        var tIndex = maxSumColIndex(X.clone().mulM(X));
+        var uIndex = maxSumColIndex(Y.clone().mulM(Y));
+
+        var t1 = X.getColumnVector(tIndex);
+        var u = Y.getColumnVector(uIndex);
+        var t = Matrix.zeros(rx, 1);
+
+        while(norm(t1.clone().sub(t)) > tolerance) {
+            var w = transposeX.mmul(u);
+            w.div(norm(w));
+            t = t1;
+            t1 = X.mmul(w);
+            var q = transposeY.mmul(t1);
+            q.div(norm(q));
+            var u = Y.mmul(q);
+        }
+
+        t = t1;
+        var num = transposeX.mmul(t);
+        var den = (t.transpose().mmul(t))[0][0];
+        var p = num.div(den);
+        var pnorm = norm(p);
+        p.div(pnorm);
+        t.mul(pnorm);
+        w.mul(pnorm);
+
+        num = u.transpose().mmul(t);
+        var b = (num.div(den))[0][0];
+        X.sub(t.mmul(p.transpose()));
+        Y.sub(t.mmul(q.transpose()).mul(b));
+
+
+        T.addColumn(k, t);
+        P.addColumn(k, p);
+        U.addColumn(k, u);
+        Q.addColumn(k, q);
+        W.addColumn(k, w);
+        B[k][k] = b;
+        k++;
+    }
+
+    // NOTE: some variables commented because maybe it's needed
+    // in the future
+
+    k--;
+    n--;
+    //T = T.subMatrix(0, n, 0, k);
+    P = P.subMatrix(0, n, 0, k);
+    //U = U.subMatrix(0, n, 0, k);
+    Q = Q.subMatrix(0, n, 0, k);
+    //W = W.subMatrix(0, n, 0, k);
+    B = B.subMatrix(0, k, 0, k);
+
+    // this.T = T;
+    // this.P = P;
+    // this.U = U;
+    // this.Q = Q;
+    // this.W = W;
+    // this.B = B;
+    this.PBQ = P.mmul(B).mmul(Q.transpose());
+};
 
 /**
  * Load a PLS model from an Object
@@ -163,7 +167,7 @@ PLS.load = function (model) {
     if(model.modelName !== 'PLS')
         throw new RangeError("The current model is invalid!");
 
-    return new PLS(model, null, true);
+    return new PLS(true, model);
 };
 
 /**
--- test/test.js
+++ test/test.js
@@ -6,7 +6,8 @@ var Matrix = require("ml-matrix");
 describe("PLS-DA algorithm", function () {
     var training = [[0.1, 0.02], [0.25, 1.01] ,[0.95, 0.01], [1.01, 0.96]];
     var predicted = [[1, 0], [1, 0], [1, 0], [0, 1]];
-    var pls = new PLS(training, predicted);
+    var pls = new PLS();
+    pls.fit(training, predicted);
 
     it("test with a pseudo-AND operator", function () {
         var result = pls.predict(training);
