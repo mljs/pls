91af76dcc86ead5a692e391c728bafffc139b5ff
JeffersonH44 <swrt_9950@hotmail.com>
2015-07-09 09:01:41 -0500

    OPLS working with test.

---
+++ src/opls.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var Matrix = require('ml-matrix');
+var Utils = require('./utils');
+
+module.exports = OPLS;
+
+function OPLS(dataset, predictions, numberOSC) {
+    var X = Matrix(dataset).clone();
+    var y = Matrix(predictions).clone();
+
+    X = Utils.featureNormalize(X).result;
+    y = Utils.featureNormalize(y).result;
+
+    var rows = X.rows;
+    var columns = X.columns;
+
+    var sumOfSquaresX = X.clone().mul(X).sum();
+    var w = X.transpose().mmul(y);
+    w.div(Utils.norm(w));
+
+    var orthoW = new Array(numberOSC);
+    var orthoT = new Array(numberOSC);
+    var orthoP = new Array(numberOSC);
+    for (var i = 0; i < numberOSC; i++) {
+        var t = X.mmul(w);
+
+        var numerator = X.transpose().mmul(t);
+        var denominator = t.transpose().mmul(t)[0][0];
+        var p =  numerator.div(denominator);
+
+        numerator = w.transpose().mmul(p)[0][0];
+        denominator = w.transpose().mmul(w)[0][0];
+        var wOsc = p.sub(w.clone().mul(numerator / denominator));
+        wOsc.div(Utils.norm(wOsc));
+
+        var tOsc = X.mmul(wOsc);
+
+        numerator = X.transpose().mmul(tOsc);
+        denominator = tOsc.transpose().mmul(tOsc)[0][0];
+        var pOsc = numerator.div(denominator);
+
+        X.sub(tOsc.mmul(pOsc.transpose()));
+        orthoW[i] = wOsc.getColumn(0);
+        orthoT[i] = tOsc.getColumn(0);
+        orthoP[i] = pOsc.getColumn(0);
+    }
+
+    this.Xosc = X;
+
+    var sumOfSquaresXosx = this.Xosc.clone().mul(this.Xosc).sum();
+    this.R2X = 1 - sumOfSquaresXosx/sumOfSquaresX;
+
+    this.W = orthoW;
+    this.T = orthoT;
+    this.P = orthoP;
+    this.numberOSC = numberOSC;
+}
+
+OPLS.prototype.correctDataset = function (dataset) {
+    var X = Matrix(dataset).clone();
+    //X = Utils.featureNormalize(dataset).result;
+
+    var sumOfSquaresX = X.clone().mul(X).sum();
+    for (var i = 0; i < this.numberOSC; i++) {
+        var currentW = this.W.getColumnVector(i);
+        var currentP = this.P.getColumnVector(i);
+
+        var t = X.mmul(currentW);
+        X.sub(t.mmul(currentP));
+    }
+    var sumOfSquaresXosx = X.clone().mul(X).sum();
+
+    var R2X = 1 - sumOfSquaresXosx / sumOfSquaresX;
+
+    return {
+        datasetOsc: X,
+        R2Dataset: R2X
+    };
+};
--- test/pls-test.js
+++ test/pls-test.js
@@ -1,13 +1,14 @@
 "use strict";
 
 var PLS = require("..");
+var OPLS = require("..").OPLS;
 var Matrix = require("ml-matrix");
 
 describe("PLS-DA algorithm", function () {
     var training = [[0.1, 0.02], [0.25, 1.01] ,[0.95, 0.01], [1.01, 0.96]];
     var predicted = [[1, 0], [1, 0], [1, 0], [0, 1]];
     var pls = new PLS();
-    pls.fit(training, predicted);
+    pls.fit(training, predicted, 2, 1e-5);
 
     it("test with a pseudo-AND operator", function () {
         var result = pls.predict(training);
@@ -23,7 +24,7 @@ describe("PLS-DA algorithm", function () {
         var predicted = [[23], [15]];
 
         var newPls = new PLS();
-        newPls.fit(training, predicted);
+        newPls.fit(training, predicted, 3, 1e-5);
         var result = newPls.predict(training);
 
         result[0][0].should.be.equal(predicted[0][0]);
@@ -39,5 +40,47 @@ describe("PLS-DA algorithm", function () {
         (result[1][0]).should.be.greaterThan(result[1][1]);
         (result[2][0]).should.be.greaterThan(result[2][1]);
         (result[3][0]).should.be.lessThan(result[3][1]);
+    });
+
+    it("Apply OSC", function () {
+        var predicted = [[0], [0], [0], [1]];
+
+        pls.fit(training, predicted, 2, 1e-5);
+        var newTraining = pls.applyOSC(training);
+        //console.log(pls.predict([[0.1, 0.02], [0.25, 1.01]]));
+    });
+
+    it('Wine test', function () {
+        var dataset = [[7, 7, 13, 7],
+                       [4, 3, 14, 7],
+                       [10, 5, 12, 5],
+                       [16, 7, 11, 3],
+                       [13, 3, 10, 3]];
+        var predictions = [[14, 7, 8],
+                           [10, 7, 6],
+                           [8, 5, 5],
+                           [2, 4, 7],
+                           [6, 2, 4]];
+
+        var winePLS = new PLS();
+        var latentStructures = 2;
+        var tolerance = 1e-5;
+        winePLS.fit(dataset, predictions, latentStructures, tolerance);
+        var result = winePLS.predict(dataset);
+
+        result[2][0].should.be.equal(predictions[2][0]);
+        result[2][1].should.be.equal(predictions[2][1]);
+        result[2][2].should.be.equal(predictions[2][2]);
+    });
+});
+
+describe('OPLS', function () {
+    var X0 = [[-1, -1], [1, -1], [-1, 1], [1, 1]];
+    var X1 = [[-2.18, -2.18], [1.84, -0.16], [-0.48, 1.52], [0.83, 0.83]];
+    var y = [[2], [2], [0], [4]];
+
+    it('Main test', function () {
+        var opls = new OPLS(X1, y, 1);
+        opls.R2X.should.be.approximately(0.7402, 1e-1);
     });
 });
