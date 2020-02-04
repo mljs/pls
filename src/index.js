b27edb02721546db461c1196d18cb85a8f7613cd
jul <julien.wist@correounivalle.edu.co>
2020-02-04 08:56:54 -0500

    correct index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ export { PLS } from './PLS';
 export { KOPLS } from './KOPLS';
 export { OPLS } from './OPLS';
 export { OPLSNipals } from './OPLSNipals';
-export { summaryMetadata } from './summaryMetadata';
